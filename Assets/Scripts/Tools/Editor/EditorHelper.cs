604d7f30ac63997e7650fd4201d9ddfd49ab70a6
Dmitrii Cherepanov <tomatofish_@outlook.com>
2023-08-09 17:55:09 +0300

    editor helper

---
+++ Assets/Scripts/Tools/Editor/EditorHelper.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Tools
+{
+    public static class EditorHelper
+    {
+        public static IEnumerable<SerializedProperty> GetChildren(this SerializedProperty property)
+        {
+            property = property.Copy();
+            var nextElement = property.Copy();
+            bool hasNextElement = nextElement.NextVisible(false);
+            if (!hasNextElement)
+            {
+                nextElement = null;
+            }
+
+            property.NextVisible(true);
+            while (true)
+            {
+                if ((SerializedProperty.EqualContents(property, nextElement)))
+                {
+                    yield break;
+                }
+
+                yield return property;
+
+                bool hasNext = property.NextVisible(false);
+                if (!hasNext)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static System.Type GetTargetType(this SerializedObject obj)
+        {
+            if (obj == null) return null;
+
+            if(obj.isEditingMultipleObjects)
+            {
+                var c = obj.targetObjects[0];
+                return c.GetType();
+            }
+            else
+            {
+                return obj.targetObject.GetType();
+            }
+        }
+
+        /// <summary>
+        /// Gets the object the property represents.
+        /// </summary>
+        /// <param name="prop"></param>
+        /// <returns></returns>
+        public static object GetTargetObjectOfProperty(SerializedProperty prop)
+        {
+            if (prop == null) return null;
+
+            var path = prop.propertyPath.Replace(".Array.data[", "[");
+            object obj = prop.serializedObject.targetObject;
+            var elements = path.Split('.');
+            foreach (var element in elements)
+            {
+                if (element.Contains("["))
+                {
+                    var elementName = element.Substring(0, element.IndexOf("["));
+                    var index = System.Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", "").Replace("]", ""));
+                    obj = GetValue_Imp(obj, elementName, index);
+                }
+                else
+                {
+                    obj = GetValue_Imp(obj, element);
+                }
+            }
+            return obj;
+        }
+
+        public static object GetTargetObjectOfProperty(SerializedProperty prop, object targetObj)
+        {
+            var path = prop.propertyPath.Replace(".Array.data[", "[");
+            var elements = path.Split('.');
+            foreach (var element in elements)
+            {
+                if (element.Contains("["))
+                {
+                    var elementName = element.Substring(0, element.IndexOf("["));
+                    var index = System.Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", "").Replace("]", ""));
+                    targetObj = GetValue_Imp(targetObj, elementName, index);
+                }
+                else
+                {
+                    targetObj = GetValue_Imp(targetObj, element);
+                }
+            }
+            return targetObj;
+        }
+
+        /// <summary>
+        /// Gets the object that the property is a member of
+        /// </summary>
+        /// <param name="prop"></param>
+        /// <returns></returns>
+        public static object GetTargetObjectWithProperty(SerializedProperty prop)
+        {
+            var path = prop.propertyPath.Replace(".Array.data[", "[");
+            object obj = prop.serializedObject.targetObject;
+            var elements = path.Split('.');
+            foreach (var element in elements.Take(elements.Length - 1))
+            {
+                if (element.Contains("["))
+                {
+                    var elementName = element.Substring(0, element.IndexOf("["));
+                    var index = System.Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", "").Replace("]", ""));
+                    obj = GetValue_Imp(obj, elementName, index);
+                }
+                else
+                {
+                    obj = GetValue_Imp(obj, element);
+                }
+            }
+            return obj;
+        }
+
+        private static object GetValue_Imp(object source, string name)
+        {
+            if (source == null)
+                return null;
+            var type = source.GetType();
+
+            while (type != null)
+            {
+                var f = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+                if (f != null)
+                    return f.GetValue(source);
+
+                var p = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+                if (p != null)
+                    return p.GetValue(source, null);
+
+                type = type.BaseType;
+            }
+            return null;
+        }
+
+        private static object GetValue_Imp(object source, string name, int index)
+        {
+            var enumerable = GetValue_Imp(source, name) as System.Collections.IEnumerable;
+            if (enumerable == null) return null;
+            var enm = enumerable.GetEnumerator();
+            
+            for (int i = 0; i <= index; i++)
+            {
+                if (!enm.MoveNext()) return null;
+            }
+            return enm.Current;
+        }
+    }
+
+}
--- Assets/Scripts/Tools/SerializableDictionary/Editor/SerializableDictionaryDrawer.cs
+++ Assets/Scripts/Tools/SerializableDictionary/Editor/SerializableDictionaryDrawer.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Reflection;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
@@ -42,7 +41,7 @@ namespace Tools.Editor
         private void FindProperties(SerializedProperty property)
         {
             _pairs = property.FindPropertyRelative("_pairs");
-            var pairsObject = GetTargetObjectOfProperty(_pairs);
+            var pairsObject = EditorHelper.GetTargetObjectOfProperty(_pairs);
             _list = pairsObject;
         }
 
@@ -87,7 +86,7 @@ namespace Tools.Editor
 
         private void OnItemsAdded(IEnumerable<int> indexes)
         {
-            var obj = GetTargetObjectOfProperty(_property);
+            var obj = EditorHelper.GetTargetObjectOfProperty(_property);
             var methodInfo = obj.GetType().GetMethod("Add");
             var parametersArray = new object[] {default, default};
             methodInfo.Invoke(obj, parametersArray);
@@ -99,9 +98,9 @@ namespace Tools.Editor
             {
                 var keyValueSerializedProperty = _pairs.GetArrayElementAtIndex(index);
                 var keySerializedProperty = keyValueSerializedProperty.FindPropertyRelative("_key");
-                var key = GetTargetObjectOfProperty(keySerializedProperty);
+                var key = EditorHelper.GetTargetObjectOfProperty(keySerializedProperty);
                 
-                var obj = GetTargetObjectOfProperty(_property);
+                var obj = EditorHelper.GetTargetObjectOfProperty(_property);
                 var methodInfo = obj.GetType().GetMethod("Remove");
                 var parametersArray = new object[] { key };
                 methodInfo.Invoke(obj, parametersArray);
@@ -111,63 +110,6 @@ namespace Tools.Editor
         private void OnIndexChanged(int from, int to)
         {
             
-        }
-        
-        public static object GetTargetObjectOfProperty(SerializedProperty prop)
-        {
-            if (prop == null) return null;
-
-            var path = prop.propertyPath.Replace(".Array.data[", "[");
-            object obj = prop.serializedObject.targetObject;
-            var elements = path.Split('.');
-            foreach (var element in elements)
-            {
-                if (element.Contains("["))
-                {
-                    var elementName = element.Substring(0, element.IndexOf("["));
-                    var index = System.Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", "").Replace("]", ""));
-                    obj = GetValue_Imp(obj, elementName, index);
-                }
-                else
-                {
-                    obj = GetValue_Imp(obj, element);
-                }
-            }
-            return obj;
-        }
-        
-        private static object GetValue_Imp(object source, string name)
-        {
-            if (source == null)
-                return null;
-            var type = source.GetType();
-
-            while (type != null)
-            {
-                var f = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-                if (f != null)
-                    return f.GetValue(source);
-
-                var p = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
-                if (p != null)
-                    return p.GetValue(source, null);
-
-                type = type.BaseType;
-            }
-            return null;
-        }
-        
-        private static object GetValue_Imp(object source, string name, int index)
-        {
-            var enumerable = GetValue_Imp(source, name) as System.Collections.IEnumerable;
-            if (enumerable == null) return null;
-            var enm = enumerable.GetEnumerator();
-        
-            for (int i = 0; i <= index; i++)
-            {
-                if (!enm.MoveNext()) return null;
-            }
-            return enm.Current;
         }
     }
 }
